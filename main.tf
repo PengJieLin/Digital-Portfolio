c8f4c225b0fa31b53b097e4bc90176bdf66f9f61
PengJie-Bob <pengjie.lin@mail.utoronto.ca>
2026-07-02 14:07:42 -0400

    Added CI/CD w/ GitHub Actions

--- main.tf
+++ main.tf
@@ -153,3 +153,40 @@ resource "aws_lambda_permission" "api_gw_lambda" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
+
+
+// Link Github to AWS
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-deploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
+        Action    = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            # Change "your-github-username" and "your-repo-name" to match yours!
+            "token.actions.githubusercontent.com:sub" = "repo:your-github-username/your-repo-name:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "api_url" {
   description = "The endpoint URL for the API Gateway counter"
   value       = "${aws_apigatewayv2_api.http_api.api_endpoint}/counter"
 }
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
+}

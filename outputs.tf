f026976e1a1d0ddc4e8b2666003a2881425b579b
PengJie-Bob <pengjie.lin@mail.utoronto.ca>
2026-07-08 00:34:37 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 # Purpose: Outputs Website URL and APIs
 
 output "website_url" {
-  description = "The endpoint URL for the static website"
+  description = "Endpoint URL for the static website"
   value       = aws_s3_bucket_website_configuration.resume_site.website_endpoint
-}
-
-output "api_url" {
-  description = "The endpoint URL for the API Gateway counter"
-  value       = "${aws_apigatewayv2_api.http_api.api_endpoint}/counter"
 }
 
 output "github_actions_role_arn" {
   value = aws_iam_role.github_actions.arn
+}
+
+output "cloudfront_url" {
+  value       = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
+  description = "HTTPS portfolio URL"
 }
